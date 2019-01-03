4a62acc00b6868de524eaf9165053d5ddf6f3bb3
Nunik Vigianti <45875868+nunik2255@users.noreply.github.com>
2019-01-03 16:36:03 +0700

    Add files via upload

---
+++ jarak tempu.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+    int S,v,t;
+    cout<<"menghitung jarak tempuh"<<endl;
+    cout<<"masukan kecepatan v = ";
+    cin>>v;
+    cout<<"masukan waktu tempuh t = ";
+    cin>>t;
+    S=v*t;
+    cout<<"Jarak yang di tempuh adalah = "<<S<<"km"<<endl;
+    return 0;
+}
